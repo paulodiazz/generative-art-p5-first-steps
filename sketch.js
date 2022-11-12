f3eee549d18d5afcbb7dee89f76c126f314d845c
paulo diaz <paulodiaz@192.168.1.10>
2022-11-12 12:36:24 -0600

    second commit

--- sketch.js
+++ sketch.js
@@ -33,55 +33,119 @@ class Random {
 
 let R = new Random(seed);
 
-//P5
-
-function setup(){
-console.log('run')
-createCanvas(500,500);
-
+function make2DArray(cols, rows){
+  let arr = new Array(cols);
+  for (let i =0 ; i>arr.length; i++)
+    arr[i] = Array(rows);
 }
 
+//P5
+var t = 45
+var mic;
+var song;
+
+/* function preload() {
+  song = loadSound('Minero.mp3');
+}
+ */
+function setup() { 
+  /* mic= new p5.AudioIn();
+  mic.start(); */
+ 
+  
+  createCanvas(windowWidth, windowHeight, WEBGL);
+}
+var inc=.01;
+
+var start =0;
+var yoff =0;
+
 function draw(){
-    background('black');
-   
-        
+  background(175)
+  var xoff= start;
+   for (var x = start; x<width; x++){
+
+    var y = noise(xoff) * height;
+    circle(x, y);
+    xoff +=inc;
     
+   }
+
+   var x = map(noise(xoff), 0, 1 ,0 ,width);
+
+   start+=inc;
+  
 
 
-    line(100,100,100,100)
-    push()
-    
-    triangle(400,500,500,500,400,450)
-        push()
-        translate(-100,-50)
-        triangle(400,500,500,500,400,450)
-        pop()
-        push()
-        translate(-200,-100)
-        triangle(400,500,500,500,400,450)
-        pop()
-    rect(0,450,400,50);
-    rect(0,401,300,50)
-    rect(0,350,200,50)
-    pop()
+//tierra
+  for (var y = R.rango(2,10); y <= R.rango(10,15); y++) {
+    for (var x = -10; x <= 10; x++) {
+      for (var z = -4; z <= 4; z++) {
 
-   
+        push();
+        translate(20 * x, 20 * y, 20 * z);
+        rotateX(radians(t*2));
+        var boxScale = map(noise(t/2), -1, 1, 5, 10);
+        normalMaterial();
+        sphere(noise(t)*(boxScale)); //* sin(t/10));
+        pop();
 
-    var earth = (R.rango(60,90),R.rango(37,70),R.rango(3,20))
-
-
-    for(var i=400;i<500;i=i+2){
-        push()
-        stroke(earth)
-        line((i+R.rango(-5,5)),500,(i+random(-5,5)),450+(i-400))
-        
-        pop()
-        
+      }
     }
-    for(var i=0;i<298;i=i+2){
-       
-        line((i+random(-5,5)),450,(i+random(-5,5)),400)
-        stroke(74, 53, 10)
+  }
+
+
+  //agua
+
+  for (var y = 4; y <= 8; y++) {
+    for (var x = -10; x <= 0; x++) {
+      for (var z = -4; z <= 4; z++) {
+
+        push();
+        translate(20 * (x+noise(t/2)), 20 * (y+noise(t/2)), 20 * z);
+        rotateX(radians(t*2));
+        var boxScale = map(sin(t/2), -1, 1, 5, 10);
+        fill(0,0,250);
+        box(boxScale); //* sin(t/10));
+        pop();
+       push();
+        translate(20 * x, 20 * y, 20 * z);
+        rotateX(radians(t*2));
+        var boxScale = map(sin(t/2), -1, 1, 5, 10);
+        fill(250,0,0);
+        box(noise(t)*boxScale); //* sin(t/10));
+        pop();
+      }
     }
-    
+  }
+
+  //fuego
+  for (var y = -10; y <= 10; y++) {
+    for (var x = 4; x <= 10; x++) {
+      for (var z = -4; z <= 4; z++) {
+
+        push();
+        translate(20 * x, 20*y, 20 * z);
+        rotateX(radians(t*2));
+        var boxScale = map(sin(t/2), -1, 1, 5, 10);
+        fill(255);
+        box(boxScale); //* sin(t/10));
+        pop();
+      }
+    }
+  }
+
+  
+  
+  /* directionalLight(155, 100, 0, 1, -10, 0, sin(t / 2));
+  directionalLight(20, 85, 0, 1, 2, 0, cos(t / 2));
+  directionalLight(0, 0, 50, 1, 0, sin(t / 2), -2); */
+  spotLight(255, 0, 0, -1, -1, -1, 0, 0, 0)
+  
+  
+  t += 0.1;
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
